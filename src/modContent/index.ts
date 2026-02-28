b1da968196ee40aa3edd7bb53e9665858709d531
ThatGuyGW <noemail@emptydomain.com>
2026-02-28 11:01:48 +0000

    Updated Mod with Actual Details and Working Code

--- src/modContent/index.ts
+++ src/modContent/index.ts
@@ -1,31 +1,39 @@
-import { TreasureItem, Technique } from 'afnm-types';
+window.modAPI.hooks.onDeriveRecipeDifficulty((recipe, recipeStats, gameFlags) => {
 
-import icon from '../assets/image.png';
+  // Create Variable to hold Stability Bonus
+  let stabilityBonus = 0;
 
-const myTreasure: TreasureItem = {
-  kind: 'treasure',
-  name: 'The Best Treasure',
-  description: 'Wooo mod content.',
-  icon: icon,
-  stacks: 1,
-  rarity: 'mundane',
-  realm: 'coreFormation',
-};
-window.modAPI.actions.addItem(myTreasure);
+  // Get Player Control and Intensity
 
-const myTechnique: Technique = {
-  name: 'Test technique',
-  icon: '',
-  type: 'fist',
-  effects: [
-    {
-      kind: 'buffSelf',
-      buff: window.modAPI.gameData.techniqueBuffs.fist.flow,
-      amount: {
-        value: 1,
-        stat: undefined,
-      },
-    },
-  ],
-};
-window.modAPI.actions.addTechnique(myTechnique);
+    const playerControl = gameFlags.control
+    const playerIntensity = gameFlags.intensity
+
+  // For both, if above 100, get 10% of the value and flatten to nearest whole number
+
+  if (playerControl > 100) {
+    stabilityBonus += Math.floor(playerControl * 0.01);
+  }
+
+  if (playerIntensity > 100) {
+    stabilityBonus += Math.floor(playerIntensity * 0.01);
+  }
+
+  // If Stability Bonus is above 99, cap it at 99
+
+  if (stabilityBonus > 99) {
+    stabilityBonus = 99;
+  }
+
+  // Divide Stability Bonus by 100 to get a multiplier between 1 and 1.99
+
+    stabilityBonus = 1 + (stabilityBonus / 100);
+
+  // Increase stability based on calculated multiplier
+
+    recipeStats.stability *= stabilityBonus;
+
+    recipeStats.stability = Math.floor(recipeStats.stability);
+
+  return recipeStats;
+
+});
